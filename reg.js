5ada54fdaf6229ac1b7008f2f8bc999967a9a17a
gracekazungu <kazungungrace@gmail.com>
2023-06-17 13:23:36 +0300

    commit

--- reg.js
+++ reg.js
@@ -1,34 +1,43 @@
-document.getElementById("form").addEventListener("submit",async function(event){
-    event.preventDefault()
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+// document.getElementById("form").addEventListener("submit",async function(event){
+//     event.preventDefault()
     
-    let firstName=document.getElementById(`first-name`).value;
-    let secondName=document.getElementById(`second-name`).value;
-    let lastName=document.getElementById(`last-name`).value;
-    let date=document.getElementById(`date`).value;
-    let email=document.getElementById(`email`).value;
-    let data={
-        firstName: firstName,
-        secondName: secondName,
-        lastName: lastName,
-        date: date,
-        email: email,
-    };
-    console.log({data});
-    
-    
-    let result=await fetch("https://formsubmit.co/kazungungrace@gmail.com",{
-        method: "POST",
-        headers:{
-            'Content-Type':'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response=>response.json())
-    .then(response=>response)
-    .catch(error=>error.message)
-    console.log({result})
+//     let firstName=document.getElementById(`first-name`).value;
+//     let secondName=document.getElementById(`second-name`).value;
+//     let lastName=document.getElementById(`last-name`).value;
+//     let date=document.getElementById(`date`).value;
+//     let email=document.getElementById(`email`).value;
+//     let data={
+//         firstName: firstName,
+//         secondName: secondName,
+//         lastName: lastName,
+//         date: date,
+//         email: email,
+//     };
+//     console.log({data});
     
     
-    let success=document.getElementById('success');
-    result.id ? success.innerHTML='form submitted SuccessFully':success.innerHTML='form not submitted'
-    })
+//     let result=await fetch("https://formsubmit.co/kazungungrace@gmail.com",{
+//         method: "POST",
+//         headers:{
+//             'Content-Type':'application/json'
+//         },
+//         body: JSON.stringify(data)
+//     })
+//     .then(response=>response.json())
+//     .then(response=>response)
+//     .catch(error=>error.message)
+//     console.log({result})
+    
+    
+//     let success=document.getElementById('success');
+//     result.id ? success.innerHTML='form submitted SuccessFully':success.innerHTML='form not submitted'
+//     })
